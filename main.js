50fce162c8d2660b614c41ea967182acae51ac10
hanashiro <felipeaah@gmail.com>
2020-09-21 11:05:29 -0300

    aula-02-02

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 import {Game} from './scripts/game-engine/game.js';
 
+
+const p1Button = document.querySelector('#player1');
+const p2Button = document.querySelector('#player2');
+const buttonsList = document.querySelector('.buttons-list');
+
+
+p1Button.addEventListener('click', () => startGame(1));
+p2Button.addEventListener('click', () => startGame(2));
+
+
 Game.constructor();
 Promise.all([
     Game.SoundManager.loadAll([]),
     Game.ImageManager.loadAll([])
 ])
 .then(() => {
-    Game.start();
+    buttonsList.style.display = 'flex';
     
     
 });
+
+
+function startGame(players){
+    buttonsList.style.display = 'none';
+    Game.start();
+}
